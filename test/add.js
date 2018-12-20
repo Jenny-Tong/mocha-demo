f26ce7cc88af13b3a3a576a88013db61336271b8
zhangyingye <imyingye@gmail.com>
2018-12-20 17:12:28 +0800

    init project

---
+++ test/add.js
@@ -0,0 +1,11 @@
+var add = require('../src/add.js')
+var expect = require('chai').expect
+
+describe('加法函数测试', function () {
+  it('1 加 1 应该等于 2', function () {
+    expect(add(1, 1)).to.be.equal(2)
+  })
+  it('1 加 1 不应该等于 3', function () {
+    expect(add(1,1)).to.be.not.equal(3)
+  })
+})
